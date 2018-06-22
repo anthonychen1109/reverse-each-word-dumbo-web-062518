e69ef75619529a98a50440cc810163930f034fe7
Anthony Chen <chen.anthony.1109@gmail.com>
2018-06-22 13:43:22 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 def reverse_each_word(string)
   arr = []
-  arr.push(string.split(' '))
-  arr.collect do |word|
-    for i in 0...word.length
-      word[i].reverse()
+  arr.push(string.split(" "))
+  arr.each do |word|
+    word.each do |character|
+      character.reverse
     end
   end
 end
